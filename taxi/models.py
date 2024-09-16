3e4ab9b58465f3140dd58af9eb83bbf57c52ec8d
artem.puhach <artem.puhach@cyberpolice.gov.ua>
2024-09-16 17:55:17 +0300

    Add related_name for both fields

--- taxi/models.py
+++ taxi/models.py
@@ -22,5 +22,5 @@ class Driver(AbstractUser):
 
 class Car(models.Model):
     model = models.CharField(max_length=100)
-    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
-    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name="cars")
+    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="cars")
